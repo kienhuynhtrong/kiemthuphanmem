ecae73d90eb0a9be0737bbff7aa30fadd827fc0d
unknown <kienhuynhtrong@gmail.com>
2019-03-06 19:50:20 +0700

    Tao comment trong file program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace kiemthu
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Comment's;
         }
     }
 }
